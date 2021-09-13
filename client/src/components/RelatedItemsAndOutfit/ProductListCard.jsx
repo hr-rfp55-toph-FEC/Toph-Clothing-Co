4ff7bd48d7dde8774d6dc7d7ff5b9a1b299ea38b
Bishal Gautam <bgautam.se@gmail.com>
2021-09-13 16:58:58 -0700

    Organization of Card is coming together. Once the image is added, can adjust a fixed value for the dimensions of the image, and then adjust the positioning of the details div underneath.

--- client/src/components/RelatedItemsAndOutfit/ProductListCard.jsx
+++ client/src/components/RelatedItemsAndOutfit/ProductListCard.jsx
@@ -4,8 +4,17 @@ const ProductListCard = (props) => {
   const placeholder = props;
   return (
     <div className="product-list-card">
-      <div className="card-image-container"> Image</div>
-      <div className="card-details-container">Product Details</div>
+      <div className="card-image-container">
+        Fixed size image to keep content the same below
+
+      </div>
+
+      <div className="card-details-container">
+        <h5 className="category-heading">Category</h5>
+        <p>Expanded Product Name with Extra Text</p>
+        <span>$100</span>
+        <div>Rating Stars</div>
+      </div>
     </div>
   );
 };
