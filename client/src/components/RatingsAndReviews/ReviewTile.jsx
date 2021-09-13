14c207818b2174edfd2fd0a6a67474a871510dfa
Ya Liu <sarahl0216@gmail.com>
2021-09-13 13:09:54 -0700

    implemented API get request for reviews, currently rendering all reviews for a product

--- client/src/components/RatingsAndReviews/ReviewTile.jsx
+++ client/src/components/RatingsAndReviews/ReviewTile.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ReviewTile = ({ review }) => (
 
   <div className="review-tile">
+    {review.rating}
     {review.summary}
+    {review.body}
   </div>
 );
 
--- client/src/components/RatingsAndReviews/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/ReviewsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import ReviewTile from './ReviewTile';
+import config from '../../config/config';
 
 const ReviewsList = class extends React.Component {
   constructor(props) {
@@ -13,8 +14,31 @@ const ReviewsList = class extends React.Component {
     this.getReviews = this.getReviews.bind(this);
   }
 
-  getReviews () {
+  componentDidMount() {
+    this.getReviews();
+  }
 
+  getReviews() {
+    const options = {
+      url: 'reviews/',
+      method: 'get',
+      baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/',
+      headers: {
+        'User-Agent': 'request',
+        Authorization: config.TOKEN,
+      },
+      params: {
+        product_id: 40344,
+      },
+    };
+    axios(options)
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({ reviews: response.data.results });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render() {
