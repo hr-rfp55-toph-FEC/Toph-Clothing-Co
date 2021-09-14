e90dfcf6ece23e304d35cb6c8acd6ac048654670
Bishal Gautam <bgautam.se@gmail.com>
2021-09-14 09:23:48 -0700

    morning save

--- client/src/components/RelatedItemsAndOutfit/Axios.jsx
+++ client/src/components/RelatedItemsAndOutfit/Axios.jsx
@@ -1,12 +1,12 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp',
-//   timeout: 3000,
-//   headers: {
-//     Authorization: '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',
-//   },
-// });
+const api = axios.create({
+  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp',
+  timeout: 3000,
+  headers: {
+    Authorization: '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',
+  },
+});
 
-// export default api;
+export default api;
 // ESLint didnt like me importing and using this without the this keyword in a class Method.
