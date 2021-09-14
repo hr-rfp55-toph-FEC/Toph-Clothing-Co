df5c5b372f694c48ede06eb61c7478919e093246
Bishal Gautam <bgautam.se@gmail.com>
2021-09-14 13:01:51 -0700

    Refactored Client to not req to API, instead server handles all API requests.

--- client/src/components/RelatedItemsAndOutfit/Axios.jsx
+++ client/src/components/RelatedItemsAndOutfit/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: ' http://localhost:9001',
+  baseURL: ' http://localhost:9000',
   timeout: 3000,
 });
 
--- server/helpers/related.js
+++ server/helpers/related.js
@@ -13,22 +13,18 @@ module.exports = {
     .then((data) => data)
     .catch((err) => console.log(err)),
 
-  getRelatedProductsInfo: (currProdId) => {
-    this.getRelatedProductIds(currProdId)
-      .then((relatedIds) => relatedIds.data.map((id) => this.getProductInfo(id)))
-      .then((relatedProdsInfoPromises) => Promise.all(relatedProdsInfoPromises))
-      .then((response) => response.map((res) => res.data))
-      .then((relProdsInfo) => this.setState({ relatedProdsInfo: relProdsInfo }))
-      .catch((err) => console.log(err));
-  },
+  getRelatedProductsInfo: (currProdId) => module.exports.getRelatedProductIds(currProdId)
+    .then((relatedIds) => relatedIds.data.map((id) => module.exports.getProductInfo(id)))
+    .then((relatedProdsInfoPromises) => Promise.all(relatedProdsInfoPromises))
+    .then((response) => response.map((res) => res.data))
+    .then((relProdsInfo) => relProdsInfo)
+    .catch((err) => console.log(err)),
 
-  getRelatedProductStyles: (currProdId) => {
-    this.getRelatedProductIds(currProdId)
-      .then((relatedIds) => relatedIds.data.map((id) => this.getProductStyles(id)))
-      .then((relatedStylesPromises) => Promise.all(relatedStylesPromises))
-      .then((response) => response.map((res) => res.data))
-      .then((relProdStyles) => this.setState({ relatedProdStyles: relProdStyles }))
-      .catch((err) => console.log(err));
-  },
+  getRelatedProductStyles: (currProdId) => module.exports.getRelatedProductIds(currProdId)
+    .then((relatedIds) => relatedIds.data.map((id) => module.exports.getProductStyles(id)))
+    .then((relatedStylesPromises) => Promise.all(relatedStylesPromises))
+    .then((response) => response.map((res) => res.data))
+    .then((relProdStyles) => relProdStyles)
+    .catch((err) => console.log(err)),
 
 };
