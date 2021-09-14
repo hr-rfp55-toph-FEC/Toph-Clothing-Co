ab1240d98a3ace1095b93be04cfdb9b6c8614370
Bishal Gautam <bgautam.se@gmail.com>
2021-09-14 10:23:59 -0700

    Set up express server, along with config file. Server dir includes helper dir that will house all of our API call helper methods.

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express'); // npm installed
+const api = require('./helpers/api.js');
+const overview = require('./helpers/overview.js');
+const qna = require('./helpers/qna.js');
+const related = require('./helpers/related.js');
+const reviews = require('./helpers/reviews.js');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// other configuration...
+
+//Daniel
+
+
+
+
+//Alex
+
+
+
+
+
+//Ya
+
+
+
+
+
+//Bishal
+
+
+
+
+
+app.listen(9001, () => {
+  console.log('connected to server at 9001');
+});
