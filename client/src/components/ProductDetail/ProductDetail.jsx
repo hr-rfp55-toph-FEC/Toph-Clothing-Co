a606a8a701a1aabe2ed0f81f445d809cae73685b
Daniel Ho <dho1994@gmail.com>
2021-09-13 18:51:28 -0700

    Set up axios request for getting all product list

--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -1,20 +1,60 @@
 import React from 'react';
+// import axios from 'axios';
+
 import ImageGallery from './ImageGallery';
 import ProductInformation from './ProductInformation';
 import StyleSelector from './StyleSelector';
 import AddToCard from './AddToCard';
+
+// import config from '../../../../config';
 
 class ProductDetail extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      product: {
+        id: 40344,
+        campus: 'hr-rfp',
+        name: 'Camo Onesie',
+        slogan: 'Blend in to your crowd',
+        description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+        category: 'Jackets',
+        default_price: '140.00',
+        created_at: '2021-08-13T14:38:44.509Z',
+        updated_at: '2021-08-13T14:38:44.509Z',
+      },
       expanded: false,
     };
   }
 
+  // componentDidMount() {
+  //   this.getProduct();
+  // }
+
+  // getProduct() {
+  //   const options = {
+  //     method: 'get',
+  //     url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+  //     headers: {
+  //       Authorization: config.TOKEN,
+  //     },
+  //   };
+  //   axios(options)
+  //     .then((result) => {
+  //       console.log(result.data);
+  //       this.setState({
+  //         product: result.data[0],
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       throw new Error(error.message);
+  //       // console.log('Error in getting all products', error);
+  //     });
+  // }
+
   render() {
-    const { expanded } = this.state;
+    const { expanded, product } = this.state;
 
     if (expanded === true) {
       return (
@@ -29,7 +69,7 @@ class ProductDetail extends React.Component {
       <div>
         ProductDetail
         <ImageGallery />
-        <ProductInformation />
+        <ProductInformation product={product} />
         <StyleSelector />
         <AddToCard />
       </div>
--- client/src/components/ProductDetail/ProductInformation.jsx
+++ client/src/components/ProductDetail/ProductInformation.jsx
@@ -1,10 +1,23 @@
 // import React, { useState } from 'react';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ProductInformation() {
+function ProductInformation(props) {
+  const { product } = props;
+
   return (
-    <div>Product Information</div>
+    <div>
+      Product Information
+      <div id="product-rating">Star Rating (fill me in) Read all [#] reviews</div>
+      <div id="product-category">{product.category}</div>
+      <div id="product-name">{product.name}</div>
+      <div id="product-price">Price (fill me in)</div>
+    </div>
   );
 }
+
+ProductInformation.propTypes = {
+  product: PropTypes.shape.isRequired,
+};
 
 export default ProductInformation;
