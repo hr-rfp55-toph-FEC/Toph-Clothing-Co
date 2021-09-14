20b37685538b64c80c963cd47f623ceca4136624
Bishal Gautam <bgautam.se@gmail.com>
2021-09-13 18:41:21 -0700

    Parent class succesfully requests the related products of a hardcoded ID and saves that array of objects in state.

--- client/src/components/RelatedItemsAndOutfit/RelatedItemsAndOutfit.jsx
+++ client/src/components/RelatedItemsAndOutfit/RelatedItemsAndOutfit.jsx
@@ -9,37 +9,43 @@ class RelatedItemsAndOutfit extends React.Component {
     super(props);
     this.api = axios.create({
       baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp',
-      timeout: 3000,
+      timeout: 5000,
       headers: {
         Authorization: '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',
       },
     });
     this.state = {
-
+      relatedProds: [],
     };
   }
 
   componentDidMount() {
     const currProdId = '40344';
+    //hard code in a currentProdId for now
     this.getRelatedProducts(currProdId);
-
     //and info about userOutfit
   };
 
+  getProductInfo(prodId) {
+    return this.api.get(`/products/${prodId}`)
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  }
+
   getRelatedProducts(currProdId) {
-    this.api.get(`/products/${currProdId}/related`, {
-    })
-      .then((relatedIds) => {
-        console.log(relatedIds.data);
-        //upon mount I need info about related products -> related Id's returned
-      });
+    this.api.get(`/products/${currProdId}/related`)
+      .then((relatedIds) => relatedIds.data.map((id) => this.getProductInfo(id)))
+      .then((relatedProdsPromise) => Promise.all(relatedProdsPromise))
+      .then((response) => response.map((res) => res.data))
+      .then((relProds) => this.setState({ relatedProds: relProds }))
+      .catch((err) => console.log(err));
   }
 
   render() {
     return (
       <div className="related-lists">
         <div className="related-product-list-container">
-          <RelatedProducts />
+          <RelatedProducts relatedProds={this.state.relatedProds}/>
         </div>
         <div className="outfit-list-container">
           <UserOutfit />
