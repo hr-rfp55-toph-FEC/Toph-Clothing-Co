04e08672c5a0301363a5641b38fec5471da1b331
Daniel Ho <dho1994@gmail.com>
2021-09-16 12:48:53 -0700

    Styling refactor - clamp and rem everything

--- client/src/components/ProductOverview/StyleThumbnail.jsx
+++ client/src/components/ProductOverview/StyleThumbnail.jsx
@@ -1,5 +1,4 @@
-// import React, { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function StyleThumbnail(props) {
@@ -7,12 +6,25 @@ function StyleThumbnail(props) {
     style,
   } = props;
 
+  const [checkState, setCheckState] = useState(false);
+
   console.log('style', style);
+
+  function handlecheck() {
+    // console.log('click');
+    setCheckState(!checkState);
+  }
+
+  let styleThumbnailContainerClass = 'product-style-thumbnail-container';
+  styleThumbnailContainerClass += checkState ? '-checked' : '';
+  let styleThumbnailClass = 'product-style-thumbnail';
+  styleThumbnailClass += checkState ? '-checked' : '';
 
   // Default product style thumbnail to 1st photo within that style
   return (
-    <div className="product-style-thumbnail-container">
-      <img className="product-style-thumbnail" src={style.photos[0].thumbnail_url} alt="Style Thumbnail" />
+    <div onClick={handlecheck} onKeyPress={handlecheck} role="menuitem" tabIndex="0" className={styleThumbnailContainerClass}>
+      {checkState && <div className="checky-check">✓</div>}
+      <img className={styleThumbnailClass} src={style.photos[0].thumbnail_url} alt="Style Thumbnail" />
     </div>
   );
 }
