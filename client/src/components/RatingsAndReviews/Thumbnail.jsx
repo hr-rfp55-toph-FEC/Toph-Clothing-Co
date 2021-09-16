9d2b9a8dded4f5ab2a49b7bb0a9bfa1a427d555c
Ya Liu <sarahl0216@gmail.com>
2021-09-15 21:13:49 -0700

    initial attempt of modal, functionality complete, tweaking css

--- client/src/components/RatingsAndReviews/Thumbnail.jsx
+++ client/src/components/RatingsAndReviews/Thumbnail.jsx
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types';
 
 const Thumbnail = ({ photo }) => {
   const [showModal, setShowModal] = useState(false);
+  const thumbnail = (<img className="thumbnail" src={photo.url} alt="review-attachment" width="100px" role="presentation" onClick={() => setShowModal(true)} />);
 
-  return (
-    <img className="thumbnail" src={photo.url} alt="review-attachments" width="100px" role="presentation" onClick={() => setShowModal(true)} />
+  const modal = (
+    <div id="review-modal" className="modal-container">
+      <span className="closeimg" onClick={() => setShowModal(false)} role="presentation">x</span>
+      <img className="modal-content" src={photo.url} alt="fullsize" />
+      <div id="modal-caption">User photo</div>
+    </div>
   );
+
+  if (showModal) {
+    return modal;
+  }
+
+  return thumbnail;
 };
 
 Thumbnail.propTypes = {
