bd34f1502460cbc86a5ec192260938d9ce1727e2
Bishal Gautam <bgautam.se@gmail.com>
2021-09-16 13:43:52 -0700

    made changes requests for pull request

--- client/src/components/RelatedItemsAndOutfit/UserOutfits/AddOutFitCard.jsx
+++ client/src/components/RelatedItemsAndOutfit/UserOutfits/AddOutFitCard.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AddOutfitCard = ({ addToCurrOutfits }) => {
-  const makeLintHappy = addToCurrOutfits;
-  return (
-    <div className="product-list-card">
-      <div className="card-image-container">
-        <button onClick={addToCurrOutfits} type="button" className="card-button">+</button>
-      </div>
-      <div className="card-details-container">
-        <h6 className="category-heading">
-          ADD CURRENT PRODUCT TO OUTFIT
-        </h6>
-        <p className="product-details">
-        </p>
-      </div>
+const AddOutfitCard = ({ addToCurrOutfits }) => (
+  <div className="product-list-card">
+    <div className="card-image-container">
+      <button onClick={addToCurrOutfits} type="button" className="card-button">+</button>
     </div>
-  );
-};
+    <div className="card-details-container">
+      <h6 className="category-heading">
+        ADD CURRENT PRODUCT TO OUTFIT
+      </h6>
+      <p className="product-details">
+      </p>
+    </div>
+  </div>
+);
 
 AddOutfitCard.propTypes = {
   addToCurrOutfits: PropTypes.instanceOf(Function).isRequired,
