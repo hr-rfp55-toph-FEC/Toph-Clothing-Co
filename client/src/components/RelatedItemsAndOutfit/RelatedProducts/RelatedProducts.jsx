6c3531eb1bfc5d7eb1bf5adbd8af63cba0c30e77
Bishal Gautam <bgautam.se@gmail.com>
2021-09-15 17:52:21 -0700

    started work on carousel

--- client/src/components/RelatedItemsAndOutfit/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedItemsAndOutfit/RelatedProducts/RelatedProducts.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RelatedCarousel from './RelatedCarousel';
+import Carousel from './Carousel';
 
 const RelatedProducts = ({ prodsInfo, prodsStyles, prodsMeta }) => (
   <div>
     Related Products Here:
-    <RelatedCarousel prodsInfo={prodsInfo} prodsStyles={prodsStyles} prodsMeta={prodsMeta} />
+    {/* <RelatedCarousel prodsInfo={prodsInfo}
+    prodsStyles={prodsStyles} prodsMeta={prodsMeta} /> */}
+    <Carousel>
+
+    </Carousel>
   </div>
 );
 
 RelatedProducts.propTypes = {
-  prodsInfo: PropTypes.instanceOf(Array).isRequired,
-  prodsStyles: PropTypes.instanceOf(Array).isRequired,
-  prodsMeta: PropTypes.instanceOf(Array).isRequired,
+  prodsInfo: PropTypes.instanceOf(Object).isRequired,
+  prodsStyles: PropTypes.instanceOf(Object).isRequired,
+  prodsMeta: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default RelatedProducts;
