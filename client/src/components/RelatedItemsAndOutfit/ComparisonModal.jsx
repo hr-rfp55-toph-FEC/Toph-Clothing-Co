09a079c0ac781173c6966f0c4f490f6ace5545b3
Bishal Gautam <bgautam.se@gmail.com>
2021-09-17 19:28:21 -0700

    refactored model to use class based, still has error. Just realized I was passing in the Star function, instead of invoking it JSX style lmao

--- client/src/components/RelatedItemsAndOutfit/ComparisonModal.jsx
+++ client/src/components/RelatedItemsAndOutfit/ComparisonModal.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Stars from '../Stars';
 
-const ComparisonModal = ({ closeModalHandler, modalClass, currProd, modalRelProd }) => {
-  const [characteristics, setCharacteristics] = useState(['']);
-  const [currProdEntries, setCurrProdEntries] = useState(['']);
-  const [relProdEntries, setRelProdEntries] = useState(['']);
+class ComparisonModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characteristics: [''],
+      currProdEntries: [''],
+      relProdEntries: [''],
+    };
+  }
 
-  const rowItemDiv = (prodChar, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{prodChar}</h3></div>;
-  const noValue = (orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell" />;
-  const characteristicList = (char, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{char}</h3></div>;
-
-  const shouldDisplayModalData = Object.keys(modalRelProd).length !== 0;
-  useEffect(() => {
-    if (shouldDisplayModalData) {
+  componentDidUpdate(prevProps) {
+    const {
+      currProd, modalRelProd,
+    } = this.props;
+    if (modalRelProd !== prevProps.modalRelProd) {
       const currProdKey = Object.keys(currProd)[0];
       const relProdKey = Object.keys(modalRelProd)[0];
       const currEntries = currProd[currProdKey];
@@ -20,50 +24,145 @@ const ComparisonModal = ({ closeModalHandler, modalClass, currProd, modalRelProd
       const currProdChars = Object.keys(currProd[currProdKey]);
       const relProdChars = Object.keys(modalRelProd[relProdKey]);
       const allChars = Array.from(new Set([...currProdChars, ...relProdChars]));
-      setCharacteristics(allChars);
-      setCurrProdEntries(Object.entries(currEntries));
-      setRelProdEntries(Object.entries(relEntries));
+      this.setTheState(allChars, currEntries, relEntries);
     }
-  }, [shouldDisplayModalData, currProd, modalRelProd]);
+  }
 
-  return (
-    <div className={modalClass}>
-      <div className="comp-modal-content">
-        <button type="button" onClick={closeModalHandler} className="comp-close-modal">&times;</button>
-        <h1 className="comp-modal-header">Comparing</h1>
-        <div className="comp-table comp-table--3cols">
+  setTheState(allChars, currEntries, relEntries) {
+    this.setState({
+      characteristics: allChars,
+      currProdEntries: Object.entries(currEntries),
+      relProdEntries: Object.entries(relEntries),
+    });
+  }
 
-          <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(currProd)[0]}</h3></div>
-          {characteristics.map((characteristic, index) => {
-            for (let i = 0; i < currProdEntries.length; i += 1) {
-              if (currProdEntries[i][0] === characteristic) {
-                const charVal = currProdEntries[i][1].value;
-                return rowItemDiv(charVal, index + 1);
-              }
-            }
-            return noValue(index + 1);
-          })}
+  render() {
+    const {
+      closeModalHandler, modalClass, currProd, modalRelProd,
+    } = this.props;
 
-          <div style={{ order: '0' }} className="comp-table-cell" />
-          {characteristics.map((characteristic, index) => characteristicList(characteristic,
-            index + 1))}
+    const rowItemDiv = (prodChar, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{prodChar}</h3></div>;
+    const noValue = (orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell" />;
+    const characteristicList = (char, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{char}</h3></div>;
 
-          <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(modalRelProd)[0]}</h3></div>
-          {characteristics.map((characteristic, index) => {
-            for (let i = 0; i < relProdEntries.length; i += 1) {
-              if (relProdEntries[i][0] === characteristic) {
-                const charVal = relProdEntries[i][1].value;
-                return rowItemDiv(charVal, index + 1);
-              }
-            }
-            return noValue(index + 1);
-          })}
+    const { characteristics, currProdEntries, relProdEntries } = this.state;
 
+    if (Object.keys(modalRelProd).length !== 0) {
+      return (
+        <div className={modalClass}>
+          <div className="comp-modal-content">
+            <button type="button" onClick={closeModalHandler} className="comp-close-modal">&times;</button>
+            <h1 className="comp-modal-header">Comparing</h1>
+            <div className="comp-table comp-table--3cols">
+
+              <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(currProd)[0]}</h3></div>
+              {characteristics.map((characteristic, index) => {
+                for (let i = 0; i < currProdEntries.length; i += 1) {
+                  if (currProdEntries[i][0] === characteristic) {
+                    const charVal = currProdEntries[i][1].value;
+                    const prodId = `BGM-${currProdEntries[i][1].id}`;
+                    return rowItemDiv(Stars(charVal, prodId), index + 1);
+                    return rowItemDiv(charVal, index + 1);
+                  }
+                }
+                return noValue(index + 1);
+              })}
+
+              <div style={{ order: '0' }} className="comp-table-cell" />
+              {characteristics.map((characteristic, index) => characteristicList(characteristic,
+                index + 1))}
+
+              <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(modalRelProd)[0]}</h3></div>
+              {characteristics.map((characteristic, index) => {
+                for (let i = 0; i < relProdEntries.length; i += 1) {
+                  if (relProdEntries[i][0] === characteristic) {
+                    const charVal = relProdEntries[i][1].value;
+                    const prodId = `BGM-${relProdEntries[i][1].id}`;
+                    return rowItemDiv(Stars(charVal, prodId), index + 1);
+                    return rowItemDiv(charVal, index + 1);
+                  }
+                }
+                return noValue(index + 1);
+              })}
+
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      );
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+// const ComparisonModal = ({ closeModalHandler, modalClass, currProd, modalRelProd }) => {
+//   const [characteristics, setCharacteristics] = useState(['']);
+//   const [currProdEntries, setCurrProdEntries] = useState(['']);
+//   const [relProdEntries, setRelProdEntries] = useState(['']);
+
+//   // const shouldDisplayModalData = Object.keys(modalRelProd).length !== 0;
+//   useEffect(() => {
+//     if (Object.keys(modalRelProd).length !== 0) {
+//       const currProdKey = Object.keys(currProd)[0];
+//       const relProdKey = Object.keys(modalRelProd)[0];
+//       const currEntries = currProd[currProdKey];
+//       const relEntries = modalRelProd[relProdKey];
+//       const currProdChars = Object.keys(currProd[currProdKey]);
+//       const relProdChars = Object.keys(modalRelProd[relProdKey]);
+//       const allChars = Array.from(new Set([...currProdChars, ...relProdChars]));
+//       setCharacteristics(allChars);
+//       setCurrProdEntries(Object.entries(currEntries));
+//       setRelProdEntries(Object.entries(relEntries));
+//     }
+//   }, [currProd, modalRelProd]);
+
+//   const rowItemDiv = (prodChar, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{prodChar}</h3></div>;
+//   const noValue = (orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell" />;
+//   const characteristicList = (char, orderNumber) => <div style={{ order: `${orderNumber}` }} className="comp-table-cell"><h3>{char}</h3></div>;
+
+//   return (
+//     <div className={modalClass}>
+//       <div className="comp-modal-content">
+//         <button type="button" onClick={closeModalHandler} className="comp-close-modal">&times;</button>
+//         <h1 className="comp-modal-header">Comparing</h1>
+//         <div className="comp-table comp-table--3cols">
+
+//           <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(currProd)[0]}</h3></div>
+//           {characteristics.map((characteristic, index) => {
+//             for (let i = 0; i < currProdEntries.length; i += 1) {
+//               if (currProdEntries[i][0] === characteristic) {
+//                 const charVal = currProdEntries[i][1].value;
+//                 const prodId = `BGM-${currProdEntries[i][1].id}`;
+//                 return rowItemDiv(Stars(charVal, prodId), index + 1);
+//                 return rowItemDiv(charVal, index + 1);
+//               }
+//             }
+//             return noValue(index + 1);
+//           })}
+
+//           <div style={{ order: '0' }} className="comp-table-cell" />
+//           {characteristics.map((characteristic, index) => characteristicList(characteristic,
+//             index + 1))}
+
+//           <div style={{ order: '0' }} className="comp-table-cell"><h3>{Object.keys(modalRelProd)[0]}</h3></div>
+//           {characteristics.map((characteristic, index) => {
+//             for (let i = 0; i < relProdEntries.length; i += 1) {
+//               if (relProdEntries[i][0] === characteristic) {
+//                 const charVal = relProdEntries[i][1].value;
+//                 const prodId = `BGM-${relProdEntries[i][1].id}`;
+//                 return rowItemDiv(Stars(charVal, prodId), index + 1);
+//                 return rowItemDiv(charVal, index + 1);
+//               }
+//             }
+//             return noValue(index + 1);
+//           })}
+
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
 export default ComparisonModal;
 
 ComparisonModal.propTypes = {
