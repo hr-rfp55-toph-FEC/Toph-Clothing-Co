647236f194ec592aa9a22c0e0cd378457139a68e
Ya Liu <sarahl0216@gmail.com>
2021-09-20 18:29:30 -0700

    initial attempt to add mouse over effects on add review stars

--- client/src/components/RatingsAndReviews/AddReviewForm.jsx
+++ client/src/components/RatingsAndReviews/AddReviewForm.jsx
@@ -2,22 +2,33 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const AddReviewForm = ({ productInfo, showAddReviewModal, closeReviewFormHandler }) => {
+  const starIds = [1, 2, 3, 4, 5];
   let reviewFormClass = 'review-form-container';
 
   if (showAddReviewModal) {
     reviewFormClass = 'review-form-container show-review-form';
   }
 
-  const [innerWidth, setInnerWidth] = useState('0');
+  const [innerWidth, setInnerWidth] = useState('0%');
   const innerStarStyle = {
     width: innerWidth,
+  };
+
+  const handleStarHover = (id) => {
+    // console.log(`moused over star ${id}`);
+    setInnerWidth(String(id * 20).concat('%'));
+  };
+
+  const handleMouseLeave = () => {
+    // console.log('mouse left star');
+    setInnerWidth('0%');
   };
 
   return (
     <div className={reviewFormClass}>
       <div className="review-form-content">
-        <span className="close-modal" onClick={() => closeReviewFormHandler()} role="presentation"><i className="fas fa-times" /></span>
-        <h1>Write Your Review</h1>
+        <span className="close-modal" onClick={closeReviewFormHandler} role="presentation"><i className="fas fa-times" /></span>
+        <h1 className="review-form-title">Write Your Review</h1>
         <h3>
           About the
           {' '}
@@ -25,14 +36,18 @@ const AddReviewForm = ({ productInfo, showAddReviewModal, closeReviewFormHandler
         </h3>
         <div className="pending-stars">
           <div className="stars-outer">
-            <i className="far fa-star" />
-            <i className="far fa-star" />
-            <i className="far fa-star" />
-            <i className="far fa-star" />
-            <i className="far fa-star" />
+            {starIds.map((id) => (
+              <i
+                className="far fa-star"
+                key={id}
+                onFocus={() => handleStarHover(id)}
+                onMouseOver={() => handleStarHover(id)}
+                onMouseLeave={handleMouseLeave}
+              />
+            ))}
             <div className="stars-inner" style={innerStarStyle}>
-              <i className="fas fa-star" onMouseEnter={() => { setInnerWidth('20%'); }} />
-              <i className="fas fa-star" onMouseEnter={() => { setInnerWidth('40%'); }} />
+              <i className="fas fa-star" />
+              <i className="fas fa-star" />
               <i className="fas fa-star" />
               <i className="fas fa-star" />
               <i className="fas fa-star" />
