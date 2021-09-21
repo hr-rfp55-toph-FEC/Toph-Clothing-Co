2acb0599710cae3c5ab60ed53acdb5680eeb9a6a
Daniel Ho <dho1994@gmail.com>
2021-09-21 12:17:21 -0700

    Changed product id from 30348 to 30444.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currProdId: '40348',
+      currProdId: '40344',
       prodInfo: {},
       prodStyles: {},
       prodStyleSelected: {},
