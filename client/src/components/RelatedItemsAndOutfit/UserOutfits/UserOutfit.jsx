32a0c7bdf78df307301639c45ba9222a2aeab21b
Bishal Gautam <bgautam.se@gmail.com>
2021-09-22 00:39:53 -0700

    data persistence of user outfits using localStorage API done

--- client/src/components/RelatedItemsAndOutfit/UserOutfits/UserOutfit.jsx
+++ client/src/components/RelatedItemsAndOutfit/UserOutfits/UserOutfit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from '../Carousel';
 import AddOutfitCard from './AddOutFitCard';
@@ -7,8 +7,15 @@ import ListCard from '../ListCard';
 const UserOutfit = ({ currProd, changeProductHandler, prodStyleSelected }) => {
   const [currOutfits, setCurrOutfits] = useState([]);
   const [styleIds, setStyleIds] = useState([]);
-  // const prodStyleSelected = prodStyleSelected.style_id;
-  // console.log(prodStyleSelected);
+
+  useEffect(() => {
+    if (!currOutfits.length) {
+      const currStorage = [];
+      Object.entries(localStorage).forEach((entry) => currStorage.push(JSON.parse(entry[1])));
+      setCurrOutfits(currStorage);
+    }
+  }, [currOutfits.length]);
+
   const addToCurrOutfits = (e) => {
     e.preventDefault();
     const currProdStyleId = prodStyleSelected.style_id;
@@ -23,13 +30,17 @@ const UserOutfit = ({ currProd, changeProductHandler, prodStyleSelected }) => {
     const [product, styles, metaData] = currProd;
     const prod = [product, prodStyleSelected, metaData];
     setCurrOutfits((oldList) => [...oldList, prod]);
+    window.localStorage.setItem(currProdStyleId, JSON.stringify(prod));
   };
+
   const removeOutfit = (idToRemove) => {
     const adjustedCurrOutFits = currOutfits.filter((outfit) => outfit[1].style_id !== idToRemove);
     const adjustedSelectedStyleIds = styleIds.filter((id) => id !== idToRemove);
     setCurrOutfits(adjustedCurrOutFits);
     setStyleIds(adjustedSelectedStyleIds);
+    window.localStorage.removeItem(idToRemove);
   };
+
   return (
     <div className="outfit-list-container">
       <h2 className="user-outfit-header" id="ratings-reviews-title">YOUR OUTFIT</h2>
