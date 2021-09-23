90c6eba2ea63806b1cad2cf82868a8345cda679c
Ya Liu <sarahl0216@gmail.com>
2021-09-23 16:00:07 -0700

    changed product id

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currProdId: '40345',
+      currProdId: '40344',
       prodInfo: {},
       prodStyles: {},
       prodStyleSelected: {},
