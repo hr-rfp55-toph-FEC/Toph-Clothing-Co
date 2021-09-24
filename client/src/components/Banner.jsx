e8d067ebda9b9ee42e1008ef267778490fcb970b
Daniel Ho <dho1994@gmail.com>
2021-09-23 20:09:18 -0700

    Cleanup more styling

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,10 +4,8 @@ function Banner() {
   return (
     <div>
       <div className="titlebar">
-        {/* <img className="logo" src="/assets/logo.png" alt="logo Icon" /> */}
-        <img className="logo2" src="/assets/logo2.png" alt="logo Icon" />
+        <img className="logo" src="/assets/logo2.png" alt="logo Icon" />
         <input className="product-search" type="text" />
-        {/* <img className="magny" src="/assets/magny.png" alt="magny Icon" /> */}
       </div>
       <div className="mini-banner"> END OF SUMMER SALE! BUY ONE GET ONE 50% OFF!</div>
     </div>
