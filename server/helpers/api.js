5dfaf6f66accb95a7abd3ceeba6476341a7648c6
Ya Liu <sarahl0216@gmail.com>
2021-11-18 17:12:50 -0800

    working on process env TOKEN var

--- server/helpers/api.js
+++ server/helpers/api.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-// const config = require('./config/config.js');
+const config = require('./config/config.js');
 
 const api = axios.create({
   baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp',
   timeout: 3000,
   headers: {
-    Authorization: `${process.env.TOKEN}`,
-    // config.API_TOKEN,
+    Authorization: config.API_TOKEN,
+    // `${process.env.TOKEN}`,
   },
 });
 
